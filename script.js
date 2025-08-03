6dec279458cdd0fe2a86f40e900a85a230b1b1d2
Peshal Bhandari <peshalbhandari@Peshals-MacBook-Pro.local>
2025-08-03 02:08:57 -0500

    adding css and more context

---
+++ script.js
@@ -0,0 +1,17 @@
+const form = document.querySelector("form");
+
+if (form) {
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const name = form.querySelector("input[type='text']").value;
+    const email = form.querySelector("input[type='email']").value;
+    const message = form.querySelector("textarea").value;
+
+    if (name && email && message) {
+      alert("Thank you, " + name + "! We'll get back to you soon.");
+      form.reset();
+    } else {
+      alert("Please fill out all fields.");
+    }
+  });
+}
